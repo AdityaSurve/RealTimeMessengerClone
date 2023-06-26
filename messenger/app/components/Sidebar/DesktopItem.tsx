cb45a08ce46e458a07e62bb7e7752634af70dc5f
AdityaSurve <surveaditya521@gmail.com>
2023-06-26 10:53:37 +0530

    getCurrentUser and getSession

--- messenger/app/components/Sidebar/DesktopItem.tsx
+++ messenger/app/components/Sidebar/DesktopItem.tsx
@@ -25,8 +25,8 @@ const DesktopItem: React.FC<DesktopItemProps> = ({
       <Link
         href={href}
         className={clsx(
-          "group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100",
-          active && "bg-gray-100 text-black"
+          "group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold  hover:text-black hover:bg-gray-100",
+          active ? "bg-gray-100 text-black" : "text-gray-500"
         )}
       >
         <Icon className="w-6 h-6 shrink-0" />
