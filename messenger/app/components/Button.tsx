04f82441d86e4c30ff62a6bb51fd946b4721c1d6
AdityaSurve <surveaditya521@gmail.com>
2023-06-28 16:12:09 +0530

    ConfirmModal useActiveList

--- messenger/app/components/Button.tsx
+++ messenger/app/components/Button.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import clsx from "clsx";
-
 interface ButtonProps {
-  type: "button" | "submit" | "reset" | undefined;
+  type?: "button" | "submit" | "reset" | undefined;
   fullWidth?: boolean;
   children?: React.ReactNode;
   onClick?: () => void;
@@ -11,9 +8,8 @@ interface ButtonProps {
   danger?: boolean;
   disabled?: boolean;
 }
-
 const Button: React.FC<ButtonProps> = ({
-  type,
+  type = "button",
   fullWidth,
   children,
   onClick,
@@ -27,7 +23,18 @@ const Button: React.FC<ButtonProps> = ({
       type={type}
       disabled={disabled}
       className={clsx(
-        "flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
+        `
+        flex 
+        justify-center 
+        rounded-md 
+        px-3 
+        py-2 
+        text-sm 
+        font-semibold 
+        focus-visible:outline 
+        focus-visible:outline-2 
+        focus-visible:outline-offset-2 
+        `,
         disabled && "opacity-50 cursor-default",
         fullWidth && "w-full",
         secondary ? "text-gray-900" : "text-white",
@@ -42,5 +49,4 @@ const Button: React.FC<ButtonProps> = ({
     </button>
   );
 };
-
 export default Button;
